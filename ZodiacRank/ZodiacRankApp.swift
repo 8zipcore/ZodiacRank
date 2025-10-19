f6222ded494a41f9a6a964f545ca544743863ad8
SEUNGA HONG <howtofindjayu@gmail.com>
2025-10-20 01:36:26 +0900

    init

---
+++ ZodiacRank/ZodiacRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZodiacRankApp.swift
+//  ZodiacRank
+//
+//  Created by 홍승아 on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ZodiacRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
